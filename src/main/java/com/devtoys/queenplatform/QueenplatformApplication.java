2cee950f35a1ae754ea945765680c7d524e06de3
abnerandrade <aa.abnerandrade@outlook.com.br>
2026-05-27 22:29:38 -0300

    chore(project): first commit

---
+++ src/main/java/com/devtoys/queenplatform/QueenplatformApplication.java
@@ -0,0 +1,13 @@
+package com.devtoys.queenplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueenplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueenplatformApplication.class, args);
+	}
+
+}
